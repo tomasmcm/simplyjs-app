603d2bbbbddf3695ff103dbae87a3c9b21fb6252
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2015-06-20 13:47:28 +0100

    text

--- app.js
+++ app.js
@@ -1,40 +1,61 @@
 console.log('Simply.js demo!');
+simply.style('mono');
+
 var page=0;
+var text = "#### Reactivos ####\n\
+Especifique a descrição SADA\n\
+Agente é toda a entidade mergulhada num ambiente, provida de sensores e de actuadores. São julgados de acordo com o seu desempenho na realização de uma dada tarefa.\n\
+SADA (Sensores, Actuadores , Desempenho, Ambiente).\n\
+\n\
+Tipos de Ambientes\n\
+Acessíveis - agente retira do ambiente toda informação para decidir acção\n\
+Determinísticos (ou estocásticos) - evolução do ambiente pode ser determinada a partir da situação corrente.\n\
+Estáticos (ou dinâmicos) - ambiente não se altera enquanto agente decide acção.\n\
+\n\
+Sistema de produção\n\
+Cj de regras do tipo Se <condição> Então <acção>.\n\
+Interpretador de regras ou mecanismo de controlo (lista ordenada das regras).";
 
 simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Pressed ' + e.button + '!');
+  if(e.button=="up"){
+	page--;
+  	simply.body(text.substring(page*100));
+  }
+  if(e.button=="down"){
+  	page++;
+  	simply.body(text.substring(page*100));
+  }
 });
 
-simply.on('longClick', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
+// simply.on('longClick', function(e) {
+//   console.log(util2.format('long clicked $button!', e));
+//   simply.vibe();
+//   simply.scrollable(e.button !== 'select');
+// });
 
-simply.on('accelTap', function(e) {
-  console.log(util2.format('tapped accel axis $axis $direction!', e));
-  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
-});
+// simply.on('accelTap', function(e) {
+//   console.log(util2.format('tapped accel axis $axis $direction!', e));
+//   simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+// });
 
 simply.on('accelData', function(e) {
   if(e.accel.y>500){
   	page--;
-  	simply.subtitle('UP '+page);
+  	simply.body(text.substring(page*100));
   }
   if(e.accel.y<-500){
   	page++;
-  	simply.subtitle('DOWN '+page);
+  	simply.body(text.substring(page*100));
   }
   if(e.accel.x>900){
-  	simply.subtitle('RIGHT');
+  	simply.body('RIGHT');
   }
   if(e.accel.x<-900){
-  	simply.subtitle('LEFT');
+  	simply.body('LEFT');
   }
 });
 
 simply.setText({
-  title: 'Simply My Demo!',
-  body: '',
+  title: '',
+  body: text.substring(page*100),
 }, true);
