0ec0f28fdfc27060948aa8ebea909c88e7cb664d
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2015-06-20 11:09:06 +0100

    accel

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-/* global simply */
+console.log('Simply.js demo!');
 
-simply.text({
-    title: 'Hello!',
-    subtitle: 'How are you?',
-}, true);
-
-simply.on('singleClick', function() {
-   simply.body("I'm doing great!"); 
+simply.on('singleClick', function(e) {
+  console.log(util2.format('single clicked $button!', e));
+  simply.subtitle('Pressed ' + e.button + '!');
 });
+
+simply.on('longClick', function(e) {
+  console.log(util2.format('long clicked $button!', e));
+  simply.vibe();
+  simply.scrollable(e.button !== 'select');
+});
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('tapped accel axis $axis $direction!', e));
+  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+});
+
+simply.setText({
+  title: 'Simply Demo!',
+  body: 'This is a demo. Press buttons or tap the watch!',
+  subtitle: simply.accelPeek,
+}, true);
